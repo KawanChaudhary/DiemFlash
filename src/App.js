254f948a6bf46bbb9e01873e001d2d480644651f
KawanChaudhary <kawanchaudhary@gmail.com>
2022-06-07 10:56:11 +0530

    Complete Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/news";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+import "./App.css";
+import Favicon from "react-favicon";
+import Footer from "./components/footer";
+import About from "./components/About";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    progress: 0,
+  };
+
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
+  render() {
+    return (
+      <>
+        <Favicon url="../public/favicondf.ico" />
+        <Router>
+          <Navbar />
+          <LoadingBar
+            color="#f11946"
+            // #228B22
+            progress={this.state.progress}
+          />
+
+          <Switch>
+            <Route exact path="/general"><News
+            setProgress={this.setProgress}
+            key="general"
+            pageSize={12}
+            category="general"
+            country="in"
+          /></Route>
+            <Route exact path="/business">
+              <News
+                setProgress={this.setProgress}
+                key="business"
+                pageSize={12}
+                category="business"
+                country="in"
+              />
+            </Route>
+            <Route exact path="/entertainment">
+              <News
+                setProgress={this.setProgress}
+                key="entertainment"
+                pageSize={12}
+                category="entertainment"
+                country="in"
+              />
+            </Route>
+            <Route exact path="/health">
+              <News
+                setProgress={this.setProgress}
+                key="health"
+                pageSize={12}
+                category="health"
+                country="in"
+              />
+            </Route>
+            <Route exact path="/science">
+              <News
+                setProgress={this.setProgress}
+                key="science"
+                pageSize={12}
+                category="science"
+                country="in"
+              />
+            </Route>
+            <Route exact path="/technology">
+              <News
+                setProgress={this.setProgress}
+                key="technology"
+                pageSize={12}
+                category="technology"
+                country="in"
+              />
+            </Route>
+            <Route exact path="/sports">
+              <News
+                setProgress={this.setProgress}
+                key="sports"
+                pageSize={12}
+                category="sports"
+                country="in"
+              />
+            </Route>
+            <Route path="/about"><About setProgress={this.setProgress} /></Route>
+          </Switch>
+        </Router>
+        <Footer setProgress={this.setProgress} />
+      </>
+    );
+  }
 }
 
 export default App;
